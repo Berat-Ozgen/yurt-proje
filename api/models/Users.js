53940b787d5de52274b934bf5edb0682fef8e3d1
Berat Ozgen <berat.ozgnn.01@gmail.com>
2023-02-28 17:21:19 +0300

    register success

--- api/models/Users.js
+++ api/models/Users.js
@@ -1,38 +1,42 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const UserSchema = mongoose.Schema({
+const UserSchema = mongoose.Schema(
+  {
     userName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     lastName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     biyografi: {
-        type: String,
-        required: true,
-        default: '', 
+      type: String,
+      default: '',
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     image: {
-        type: String,
-        default: 'https://picsum.photos/200/200',
+      type: String,
+      default: 'https://picsum.photos/200/200',
     },
     gender: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-},{
+  },
+  {
     timestamps: true,
-})
+  }
+);
 
-const User = mongoose.model('User',UserSchema)
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
