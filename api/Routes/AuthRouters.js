cd170b2acb2cecb4fc10b6aa2bfe1f89cccf21cf
Berat Ozgen <berat.ozgnn.01@gmail.com>
2023-02-28 17:11:47 +0300

    api error solved #6

--- api/Routes/AuthRouters.js
+++ api/Routes/AuthRouters.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/Users.js');
 
-// Kayıt Olma 
+// Kayıt Olma
 router.post('/register', async (req, res) => {
   try {
     const { userName, lastName, email, password, gender } = req.body;
@@ -15,9 +15,11 @@ router.post('/register', async (req, res) => {
       password: hash,
       gender,
     });
-    const user = await newUser.save()
-    res.status(200).json(user)
+    const user = await newUser.save();
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).json(error);
   }
 });
+
+module.exports = router;
